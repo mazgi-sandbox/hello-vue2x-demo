f9ca9ceead682fefdf3606b4b86cda91dbde8426
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2018-04-23 20:19:25 +0900

    component 03

--- docs/script.js
+++ docs/script.js
@@ -11,4 +11,16 @@
       text: ''
     }
   });
+  new Vue({
+    el: '.js-component03',
+    data: {
+      input: '',
+      text: ''
+    },
+    methods: {
+      output: function(){
+        this.text = '<' + this.input + '>';
+      }
+    }
+  });
 }
